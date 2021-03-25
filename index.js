7a3cfb17762b50968004babb59ba5eb8e2b8f6be
Zach Frohardt <zfrohardt@gmail.com>
2021-03-25 08:42:28 -0700

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name: "Zach",
+    streetAddress: "123 Main St",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let obj = {...employee};
+    obj[key] = value;
+    return obj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let obj = {...employee};
+    delete obj[key];
+    return obj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
